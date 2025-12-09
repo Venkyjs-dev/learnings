f77f4156cf7c54517ab917b2356f25e3a6697fb3
Devi <devi131780@gmail.com>
2025-12-09 18:32:40 +0530

    error handling

--- express/project/src/app.js
+++ express/project/src/app.js
@@ -3,22 +3,32 @@ const express = require("express");
 const app = express();
 const { adminAuth, userAuth } = require("./middlerwares/auth");
 
-app.use("/admin", adminAuth);
+// app.use("/admin", adminAuth);
 
-app.get("/admin/getAllData", (req, res, next) => {
-  res.send("getAll data");
-});
+// app.get("/admin/getAllData", (req, res, next) => {
+//   res.send("getAll data");
+// });
 
-app.get("/admin/deleteUser", (req, res, next) => {
-  res.send("deleted user");
-});
+// app.get("/admin/deleteUser", (req, res, next) => {
+//   res.send("deleted user");
+// });
 
-app.use("/user/getUser", userAuth);
+app.use("/user", userAuth);
 
 app.get("/user/getUser", (req, res) => {
-  res.send("user found successfully");
+  try {
+    res.send("user found successfully");
+  } catch (err) {
+    res.status(404).send("somethign wrong");
+  }
 });
 
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log(err);
+    res.status(400).send("Something went Wrong");
+  }
+});
 app.listen(6666, () => {
   console.log("Server started");
 });
