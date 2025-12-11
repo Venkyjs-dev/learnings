98d792a790d48a7feaf242ffd6c4103f855d5242
Devi <devi131780@gmail.com>
2025-12-11 16:55:58 +0530

    adding schema validations

--- express/project/src/models/user.js
+++ express/project/src/models/user.js
@@ -1,21 +1,58 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      require: true,
+      minLength: 4,
+      maxLength: 50,
+      trim: true,
+    },
+    secondName: {
+      type: String,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      require: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      unique: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      trim: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Invalid");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is default about you",
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/previews/026/434/417/original/default-avatar-profile-icon-of-social-media-user-photo-vector.jpg",
+    },
+    skills: [String],
+    default: ["javascript", "react js"],
   },
-  secondName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
