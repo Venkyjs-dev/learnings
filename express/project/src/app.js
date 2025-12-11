25a3f433f18aef34920035ed8c7baf86ba5a3e92
Devi <devi131780@gmail.com>
2025-12-11 14:01:02 +0530

    Get Users

--- express/project/src/app.js
+++ express/project/src/app.js
@@ -3,19 +3,41 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json()); //this is the method to convert user json object to js object
 app.post("/signup", async (req, res) => {
   //create a new instance of the user model
-  const user = new User({
-    firstName: "Preethi",
-    lastName: "G",
-    emailId: "preethi@gamil.com",
-    password: "preethi@123",
-  });
+  console.log(req.body);
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("user signup successfully");
   } catch (err) {
     res.status(400).send("Error" + `${err.message}`);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  let userEmail = req.body.emailId;
+  try {
+    let users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User Not found");
+    } else {
+      console.log(users);
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  let userName = req.body.firstName;
+  try {
+    let user = await User.findOne({ firstName: userName });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("something went wrong");
   }
 });
 
