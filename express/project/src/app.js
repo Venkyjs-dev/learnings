e4f722b9f32748b7342cdccd067ee09ea14cc65a
Devi <devi131780@gmail.com>
2025-12-13 15:11:40 +0530

    password encrypted

--- express/project/src/app.js
+++ express/project/src/app.js
@@ -26,6 +26,24 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credentials");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.send("Login Successfully");
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
 //get users
 
 app.get("/users", async (req, res) => {
