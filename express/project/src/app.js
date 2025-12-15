e3161bf540740a588dd80e625cd8e104608a9775
Devi <devi131780@gmail.com>
2025-12-15 14:36:38 +0530

    setting expiry date to cookie

--- express/project/src/app.js
+++ express/project/src/app.js
@@ -41,8 +41,14 @@ app.post("/login", async (req, res) => {
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (isPasswordValid) {
-      const token = await jwt.sign({ _id: user._id }, "PROJECT@devTinder");
-      res.cookie("token", token);
+      const token = await jwt.sign({ _id: user._id }, "PROJECT@devTinder", {
+        expiresIn: "1h",
+      });
+      // res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true })
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 900000),
+        httpOnly: true,
+      });
       res.send("Login Successfully");
     } else {
       throw new Error("Invalid Credentials");
@@ -61,104 +67,13 @@ app.get("/profile", userAuth, async (req, res) => {
     res.status(400).send("Error : " + err.message);
   }
 });
-//get users
 
-app.get("/users", async (req, res) => {
-  let userEmail = req.body.emailId;
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
   try {
-    let users = await User.find({ emailId: userEmail });
-    if (users.length === 0) {
-      res.status(404).send("User Not found");
-    } else {
-      console.log(users);
-      res.send(users);
-    }
+    let user = req.user;
+    res.send(user.firstName + " sent connection request");
   } catch (err) {
-    res.status(400).send("something went wrong");
-  }
-});
-
-app.get("/user", async (req, res) => {
-  let userName = req.body.firstName;
-  try {
-    let user = await User.findOne({ firstName: userName });
-    if (!user) {
-      res.status(404).send("user not found");
-    } else {
-      res.send(user);
-    }
-  } catch (err) {
-    res.status(400).send("something went wrong");
-  }
-});
-
-//delete user
-app.delete("/user", async (req, res) => {
-  let userId = req.body.userId;
-  try {
-    await User.findByIdAndDelete(userId);
-    res.send("user deleted successfully");
-  } catch (err) {
-    res.status(400).send("something went wrong");
-  }
-});
-
-//update user by patch method
-
-// app.patch("/user", async (req, res) => {
-//   // let userId = req.params?.userId;
-//   let userId = req.body.userId;
-//   let data = req.body;
-//   console.log(data);
-
-//   try {
-//     let updateAllowed = ["photoUrl", "age", "skills", "gender", "about"];
-
-//     let isUpdateAllowed = Object.keys(data).every((k) =>
-//       updateAllowed.includes(k)
-//     );
-//     if (!isUpdateAllowed) {
-//       throw new Error("update not allowed");
-//     }
-//     if (data?.skills.length > 10) {
-//       throw new Error("Skill cannot be more than 10");
-//     }
-//     const user = await User.findByIdAndUpdate(userId, data);
-//     console.log(user);
-//     res.send("user updated successfully");
-//   } catch (err) {
-//     res.status(400).send("something went wrong");
-//   }
-// });
-
-app.patch("/user", async (req, res) => {
-  try {
-    const userId = req.body.userId;
-    const data = req.body;
-
-    const allowedUpdates = ["photoUrl", "age", "skills", "gender", "about"];
-
-    const updateKeys = Object.keys(data).filter((key) => key !== "userId");
-
-    const isValidUpdate = updateKeys.every((key) =>
-      allowedUpdates.includes(key)
-    );
-
-    if (!isValidUpdate) {
-      return res.status(400).send("Update not allowed");
-    }
-    if (data?.skills.length > 10) {
-      throw new Error("Skills cannot more than 10");
-    }
-
-    const user = await User.findByIdAndUpdate(userId, data, {
-      new: true,
-      runValidators: true,
-    });
-
-    res.send("user updated successfully");
-  } catch (err) {
-    res.status(400).send("something went wrong");
+    res.status(400).send("Error " + err.message);
   }
 });
 
